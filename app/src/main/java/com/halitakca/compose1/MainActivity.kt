1c9b7668be746304e3d5f4ab51f92266185648cc
Halit Akca <halitakca070@gmail.com>
2023-08-24 20:07:32 +0300

    Composable features.
    - One problem that the other block lay on the block.

--- app/src/main/java/com/halitakca/compose1/MainActivity.kt
+++ app/src/main/java/com/halitakca/compose1/MainActivity.kt
@@ -9,36 +9,45 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
 import com.halitakca.compose1.ui.theme.Compose1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "Android")
+            MainScreen()
         }
     }
-
-    // LAMBDA
-    fun test1(int: Int, myFunction: () -> Unit){
-        myFunction.invoke()
-    }
-    fun testFunctionLambda(){
-        println("TEST")
-    }
+}
 
     @Composable
-    fun Greeting(name: String) {
-        Text(text = "Hello $name!")
+    fun MainScreen(){
+        Text(text = "Hello Halit",color = Color.Blue, fontSize = 25.sp, fontWeight = FontWeight.Bold)//color = MaterialTheme.colors.onSecondary
     }
 
     //      Preview'ın altında ne yazıyorsa Preview'da görünecek O'dur.  onCreate ile bir alakası yok yani.
     @Preview(showBackground = true)
     @Composable
     fun DefaultPreview() {
-        Compose1Theme {
-            Greeting("Android")
-        }
+        Text(text = "Hello Halit",color = Color.Blue, fontSize = 25.sp, fontWeight = FontWeight.Bold)//color = MaterialTheme.colors.onSecondary
+        Text(text = "Hello WORLD",color = Color.Blue, fontSize = 25.sp, fontWeight = FontWeight.Bold)//color = MaterialTheme.colors.onSecondary
+        // Bu problemi çözmem lazım.
     }
-}
+    /* LAMBDA
+    fun test1(int: Int, myFunction: () -> Unit){
+        myFunction.invoke()
+    }
+     */
+/*
+    @Composable
+    fun Greeting(name: String) {
+        Text(text = "Hello $name!",color = Color.Blue, fontSize = 25.sp, fontWeight = FontWeight.Bold)//color = MaterialTheme.colors.onSecondary
+    }
+ */
+
+
